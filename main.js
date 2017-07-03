6c511b17fd3ad148fc79ff5c357433927b72bad2
Megan B <brown.m.j@outlook.com>
2017-07-03 08:39:41 -0500

    added fade in and out methods instead of hidden display

--- main.js
+++ main.js
@@ -38,34 +38,34 @@ function orderSlides() {
 	for (i = 0, j = 6; i < slides.length; i += 1, j -= 1) {
    $(slides[i]).css("z-index", j);
 	}
-	$(slides).addClass("hidden");
+	// $(slides).addClass("hidden");
 	showSlides(slides);
 }
 
 function showSlides(slides) {
 	let counter = 0;
 	if (counter === 0) {
-		slides.eq(counter).removeClass("hidden");
+		slides.eq(counter).fadeIn();
 	}
 	autoRotateSlides(counter, slides);
 	$("#next").click(function() {
 		if (counter === slides.length - 1) {
 			counter = 0;
-			slides.eq(counter).removeClass("hidden");
+			slides.eq(counter).fadeIn();
 		} else {
-			slides.eq(counter).addClass("hidden");
-			slides.eq(counter + 1).removeClass("hidden");
+			slides.eq(counter).fadeOut();
+			slides.eq(counter + 1).fadeIn();
 			counter += 1;
 		}
 	});
 	$("#prev").click(function() {
 		if (counter === 0) {
-			slides.eq(counter).addClass("hidden");
-			slides.eq(slides.length - 1).removeClass("hidden");
+			slides.eq(counter).fadeOut();
+			slides.eq(slides.length - 1).fadeIn();
 			counter = slides.length - 1;
 		} else {
-			slides.eq(counter).addClass("hidden");
-			slides.eq(counter - 1).removeClass("hidden");
+			slides.eq(counter).fadeOut();
+			slides.eq(counter - 1).fadeIn();
 			counter -= 1;
 		}
 	});
@@ -74,16 +74,16 @@ function showSlides(slides) {
 function autoRotateSlides(counter, slides) {
 	setInterval(function() {
 		if (counter === 0) {
-			slides.eq(counter).addClass("hidden");
-			slides.eq(counter + 1).removeClass("hidden");
+			slides.eq(counter).fadeOut();
+			slides.eq(counter + 1).fadeIn();
 			counter += 1;
 		} else if (counter === slides.length - 1) {
-			slides.eq(counter).addClass("hidden");
+			slides.eq(counter).fadeOut();
 			counter = 0;
-			slides.eq(counter).removeClass("hidden");
+			slides.eq(counter).fadeIn();
 		} else {
-			slides.eq(counter).addClass("hidden");
-			slides.eq(counter + 1).removeClass("hidden");
+			slides.eq(counter).fadeOut();
+			slides.eq(counter + 1).fadeIn();
 			counter += 1;
 		}
 	}, 3000);
